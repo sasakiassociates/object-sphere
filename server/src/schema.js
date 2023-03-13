271c4fe9aa9bd1ae0f4b1ef03d7d7ebd25f10737
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-03-13 12:15:51 -0400

    rough schema outlined

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,24 +1,57 @@
-const {gql} = require("apollo-server");
-
 const typeDefs = gql`
+
+    type Query {
+        "Grab all result clouds from a specific stream"
+        results(streamId : ID!) : [ResultCloud]
+        "Grab a specific result cloud from a specific stream type"
+        result(id : ID!) : ResultCloud
+        data(target : ID!, content : ID! ,type : ViewContentType!) : ResultCloudData
+    }
+
+    type Mutation{
+        setResults(id : ID!) : SetResultsResponse!
+        addResults(id : ID!) : SetResultsResponse!
+    }
 
     """
     A set of statuses a Content type would utilize in a View Study
     """
     enum ViewContentType {
-        """
-        The potential content item used to gather the max amount of pixels in a view
-        """
+        "The potential content item used to gather the max amount of pixels in a view"
         TARGET
-        """
-        The main blocking content item that sets the study with a current condition value
-        """
+        "The main blocking content item that sets the study with a current condition value"
         EXISTING
-        """
-        Optional content items that gives studies the ability to have multiple scenarios
-        """
+        "Optional content items that gives studies the ability to have multiple scenarios"
         PROPOSED
     }
+    
+    "A simple object used for Sasaki projects"
+    interface SasakiObject {
+        "The id related to the reference object id"
+        id: ID!
+        "An optional name for the object"
+        name : String
+    }
+    
+    "A simple container object that is mainly used for acting as an anchor point for a specific workflow type"
+    interface SasakiContainer{
+        "The id related to the reference object id"
+        id: ID!
+        "An optional name for the object"
+        name : String
+        "The items it contains"
+        data : [SasakiObject!]!
+    }
+
+    type ViewStudy implements SasakiContainer {
+        "The id related to the reference object id"
+        id: ID!
+        "An optional name for the object"
+        name : String
+        "The items it contains"
+        data : [SasakiObject!]!
+    }
+
     # schema here
     type ViewCloud {
         id: ID!
@@ -46,29 +79,20 @@ const typeDefs = gql`
         id : ID!
     }
 
+
     type CloudPoint {
         x : Float!
         y : Float!
         z : Float!
         meta : String
     }
-    type Query {
-        results : [ResultCloud]!
-        result(id : ID!) : ResultCloud
-        data(target : ID!, content : ID! ,type : ViewContentType!) : ResultCloudData
-    }
-
     type SetResultsResponse {
         success : Boolean!
         message : String
         values : [ResultCloudData!]!
     }
-
-    type Mutation{
-        setResults(id : ID!) : SetResultsResponse!
-        addResults(id : ID!) : SetResultsResponse!
-
-    }
 `;
+
+const {gql} = require("apollo-server");
 
 module.exports = typeDefs;
