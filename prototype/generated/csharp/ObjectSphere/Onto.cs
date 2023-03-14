335932d28392e2e9379e5159ce82a01eede12db0
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-03-14 19:17:13 -0400

    recursive getter for onto

--- prototype/generated/csharp/ObjectSphere/Onto.cs
+++ prototype/generated/csharp/ObjectSphere/Onto.cs
@@ -7,8 +7,7 @@ namespace Sasaki.Objects
 
   public abstract class Onto
   {
-    public readonly bool isOnto = true;
-    public static readonly string[] __internals = new[] {"__internals", "isOnto"};
+    public static readonly string[] __internals = {"__internals", "isOnto"};
 
     Dictionary<string, object> _objects = new Dictionary<string, object>();
 
@@ -22,22 +21,69 @@ namespace Sasaki.Objects
           if(!__internals.Any(x => x.Equals(kvp.Key))) data.Add(kvp.Value);
 
         }
+
         return data;
+
 
       }
     }
 
-    public TObj Get<TObj>(string name)
+    /// <summary>
+    /// Recursive search through all objects in the class
+    /// </summary>
+    /// <param name="name">the key to find</param>
+    /// <param name="depth">the max child depth to search</param>
+    /// <typeparam name="TObj"></typeparam>
+    /// <returns></returns>
+    public TObj Get<TObj>(string name, int depth = 10)
     {
       var obj = this[name];
 
-      if(obj is TObj casted)
+      if(obj is TObj casted) return casted;
+
+      var index = 0;
+      foreach(var kvp in _objects)
       {
-        return casted;
+        var child = Traverse<TObj>(kvp.Value, name, depth, ref index);
+        if(child != null) return child;
       }
 
-      return default;
+      return default(TObj);
     }
+
+    static TObj Traverse<TObj>(object item, string name, int depth, ref int index)
+    {
+      // 0. for each traverse we assume this is a new layer of the 
+      index++;
+      // 1. check if first try worked 
+      if(item is TObj casted) return casted;
+
+      // 2. check if we have hit the max depth
+      if(index >= depth) return default(TObj);
+
+      // 3. handle potential collections
+      if(item is Onto onto)
+      {
+        var o = onto[name];
+        if(o is TObj itemCast) return itemCast;
+
+        foreach(var oChild in onto.children)
+        {
+          var childObj = Traverse<TObj>(oChild, name, depth, ref index);
+          if(childObj != null) return childObj;
+        }
+      }
+      else if(item.IsList(out var items))
+      {
+        foreach(var child in items)
+        {
+          var childObj = Traverse<TObj>(child, name, depth, ref index);
+          if(childObj != null) return childObj;
+        }
+      }
+      return default(TObj);
+    }
+
 
     public object this[string key]
     {
