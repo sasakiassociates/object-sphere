d75002c86317352b8744c9a6b56fb812f45e99bf
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-03-15 17:16:29 -0400

    extensions for json

---
+++ prototype/generated/csharp/ObjectSphere/ObjectSphere.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Sasaki.Objects
+{
+
+  public static class ObjectSphere
+  {
+    public static DefaultContractResolver resolver => new DefaultContractResolver()
+    {
+      NamingStrategy = new SnakeCaseNamingStrategy
+      {
+        ProcessDictionaryKeys = true,
+        OverrideSpecifiedNames = true,
+        ProcessExtensionDataNames = false
+      },
+    };
+
+    public static JsonSerializerSettings settings => new JsonSerializerSettings()
+    {
+      ContractResolver = resolver,
+      Formatting = Formatting.Indented,
+      TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+    };
+
+    public static TOn Deserialize<TOn>(string obj) where TOn : Onto
+    {
+      return JsonConvert.DeserializeObject<TOn>(obj);
+    }
+
+    public static string Serialize<TOn>(TOn obj) where TOn : Onto
+    {
+      return JsonConvert.SerializeObject(obj, new JsonSerializerSettings(settings));
+    }
+  }
+
+}
