d957d48198e7945fd4ef065612135b9937e26e97
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-05-02 11:41:22 -0400

    added unversionsed

---
+++ packages/csharp/objects/ViewObjects/ViewObjectOnTo/ViewStudyOnto.cs
@@ -0,0 +1,22 @@
+﻿using Stratosphere;
+using ViewObjects;
+using ViewObjects.Studies;
+
+namespace Sasaki.Objects.ViewObjects.Onto
+{
+
+  public class ViewStudyOnto : OntoNode, IViewStudy
+  {
+
+    public ViewStudyOnto()
+    {
+      ViewId = Guid.NewGuid().ToString();
+    }
+
+    public string ViewId { get; }
+    public string? ViewName { get; set; }
+    public List<IViewObject>? objects { get; set; } = new List<IViewObject>();
+  }
+
+
+}
